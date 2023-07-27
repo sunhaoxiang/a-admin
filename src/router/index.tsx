3fa075508bfdaea6d1ca3a6694282d5dff814ebc
sunhx <673686754@qq.com>
2023-07-27 21:54:49 +0800

    feat: update router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,32 +1,8 @@
-import { Navigate } from 'react-router-dom'
-import Login from '@/pages/user/login/Login'
-import NotFound from '@/pages/NotFound'
+import { useRoutes } from 'react-router-dom'
+import { rootRouter } from './routes.tsx'
 
-export const rootRouter = [
-  {
-    path: '/',
-    element: <Navigate to="/dashboard" replace={true} />
-  },
-  {
-    path: '/login',
-    element: <Login />,
-    meta: {
-      title: 'Login',
-      key: 'login',
-      auth: false
-    }
-  },
-  {
-    path: '/404',
-    element: <NotFound />,
-    meta: {
-      title: 'Not Found',
-      key: 'notfound',
-      auth: false
-    }
-  },
-  {
-    path: '*',
-    element: <Navigate to="/404" replace={true} />
-  }
-]
+function Router() {
+  return useRoutes(rootRouter)
+}
+
+export default Router
---
+++ src/router/routes.tsx
@@ -0,0 +1,32 @@
+import { Navigate } from 'react-router-dom'
+import Login from '@/pages/user/login/Login.tsx'
+import NotFound from '@/pages/NotFound.tsx'
+
+export const rootRouter = [
+  {
+    path: '/',
+    element: <Navigate to="/dashboard" replace={true} />
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    meta: {
+      title: 'Login',
+      key: 'login',
+      auth: false
+    }
+  },
+  {
+    path: '/404',
+    element: <NotFound />,
+    meta: {
+      title: 'Not Found',
+      key: 'notfound',
+      auth: false
+    }
+  },
+  {
+    path: '*',
+    element: <Navigate to="/404" replace={true} />
+  }
+]
