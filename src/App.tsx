b247f5e4bad9d01079be721082f83392641ccff0
sunhx <673686754@qq.com>
2023-08-07 22:09:43 +0800

    feat: fix antd and tailwind style conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
+import useFixAntd from '@/hooks/useFixAntd.ts'
 import router from '@/router'
 
 function App() {
+  useFixAntd()
+
   return (
     <>
       <RouterProvider router={router} />
