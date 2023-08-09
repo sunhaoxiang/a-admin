a8642dc6398f86374f762926036e6a244dcbe768
sunhx <673686754@qq.com>
2023-08-09 20:05:36 +0800

    feat: fixed antd and tailwind style issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
-import useFixAntd from '@/hooks/useFixAntd.ts'
 import router from '@/router'
 
 function App() {
-  useFixAntd()
-
   return (
     <>
       <RouterProvider router={router} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { StyleProvider } from '@ant-design/cssinjs'
 import App from './App.tsx'
 import '@/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StyleProvider hashPriority="high">
+      <App />
+    </StyleProvider>
   </React.StrictMode>
 )
